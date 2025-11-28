572c2c2725131459830be6eaf367a2422c0edf41
yuiko-pigeon <ys.pigeon@hotmail.co.jp>
2025-11-28 21:45:44 +0900

    404リンク調整

--- 404.php
+++ 404.php
@@ -7,9 +7,7 @@
                 <article class="p-page404__text--area u-lineheight c-article p-page404__margin--bottom">
                     お探しのページは、削除されたか、名前が変更された可能性があります。<br class="p-page404__br--tb">
                     直接アドレスを入力された場合は<br class="p-page404__br--tb">アドレスが正しく入力されているか<span class="p-page404__text--sp">、</span>もう一度ご確認ください。<br>
-                    <br>
-                    ブラウザの再読み込みを行ってもこのページが表示される場合は<span class="p-page404__text--sp">、</span><br class="p-page404__br--tb">
-                    <a href="<?php echo esc_url(home_url('/')); ?>" class="p-page404__link--bottom c-link">トップページ</a>から目的のページをお探しください。
+                    <br>ブラウザの再読み込みを行ってもこのページが表示される場合は<span class="p-page404__text--sp">、</span><br class="p-page404__br--tb"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-page404__link--bottom c-link">トップページ</a>から目的のページをお探しください。
                 </article> 
 </main>
 <?php get_sidebar(); ?>
